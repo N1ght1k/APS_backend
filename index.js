f54cf154b5fe05572e174305dbd3eddf1f235418
Andrey Ovcharov <foripoker@gmail.com>
2023-02-01 22:51:52 +0300

    sql and get from html

--- index.js
+++ index.js
@@ -17,19 +17,43 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 
+let db = new sqlite3.Database(
+  "../../IdeaProjects/APS_proj/test.db",
+  sqlite3.OPEN_READWRITE,
+  (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Connected to the chinook database.");
+  }
+);
+
+let data = [];
+
 app.get("/", (req, res) => {
   // res.status(200).json("Сервер работает");
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
+app.get("/api", (req, res) => {
+  // res.status(200).json("Сервер работает");
+  const sql =
+    "SELECT a.Name, a.Auto, a.Number, a.EPC, b.Rssi FROM Main a, Current b WHERE b.EPC=a.EPC ORDER BY b.rssi DESC";
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    // rows.forEach((row) => {
+    //   console.log(row.name);
+    // });
+    res.json(rows);
+  });
+  // res.sendFile(path.join(__dirname + "/index.html"));
+});
+
 async function startApp() {
   try {
-    let db = new sqlite3.Database("test.db", sqlite3.OPEN_READWRITE, (err) => {
-      if (err) {
-        console.error(err.message);
-      }
-      console.log("Connected to the chinook database.");
-    });
     app.listen(PORT, () => console.log("listening on port " + PORT));
   } catch (error) {
     console.log(error);
